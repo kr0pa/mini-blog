17d0d5363d8c22286a6388397c06569059710635
Piotr Górski <piotrkropagorski@gmail.com>
2023-12-28 13:37:44 +0100

    create templates and update settings file

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 class BlogView(View):
     def get(self, request):
-        return HttpResponse("Hello World!")
+        posts = Post.objects.all()
+        # return HttpResponse("Hello World!")
+        
+        return render(request, "home.html")
--- blog_project/settings.py
+++ blog_project/settings.py
@@ -43,7 +43,9 @@ ROOT_URLCONF = 'blog_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,6 +107,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
