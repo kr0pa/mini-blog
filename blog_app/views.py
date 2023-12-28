cc10549e5fa285658223df22b095d17bfdd24c98
Piotr Górski <piotrkropagorski@gmail.com>
2023-12-28 17:30:21 +0100

    modify template

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
 from django.views import View
-from django.http import HttpResponse
 from .models import Post
 
 # Create your views here.
 class BlogView(View):
     def get(self, request):
         posts = Post.objects.all()
-        # return HttpResponse("Hello World!")
-        
+
         return render(request, "home.html")
