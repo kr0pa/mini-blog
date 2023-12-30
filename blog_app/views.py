789278ec210c4e5885129d99ea26e75a2fb64a1b
Piotr Górski <piotrkropagorski@gmail.com>
2023-12-30 13:47:13 +0100

    add form and update template form

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from django.views import View
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 class BlogView(View):
     def get(self, request):
         posts = Post.objects.all()
-
         return render(request, "home.html", {"posts": posts})
     
 class BlogCreateView(View):
-    def get(self, request):
-        template= "post_new.html"
-        
-        return render(request, self.template)
+    template = "post_new.html"
+    form = PostForm()
+    
+    def get(self, request):   
+        return render(request, self.template, {"form": self.form})
